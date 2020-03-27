77d820ac2c15a75840937f085c8cebf07681715c
alisa.krupenko18@gmail.com <alisa.krupenko18@gmail.com>
2020-03-27 17:01:14 +0200

    snake

---
+++ Point.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Point
+{
+    public int x;
+    public int y;
+    public char sym;
+
+    public Point(int _x, int _y, char _sym)
+    {
+        x = _x;
+        y = _y;
+        sym = _sym;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public static void Main(string[] args)
+    {
+        Point p1 = new Point(1, 2, "o");
+        Draw(p1);
+    }
+    
+    public void Draw(Point p)
+    {        
+        Console.SetCursorPosition(p.x, p.y);
+        Console.Write(p.sym);
+    }
+    
+}
+
