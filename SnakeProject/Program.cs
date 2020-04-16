44f2e24ab785aa640c42e841939cae70abf56117
alisa.krupenko18@gmail.com <alisa.krupenko18@gmail.com>
2020-04-16 10:41:43 +0300

    Snake

--- SnakeProject/Program.cs
+++ SnakeProject/Program.cs
@@ -2,49 +2,77 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SnakeProject
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.SetWindowSize(80,25);
-            /*Point p2 = new Point(3, 2, '*');
-            Point p3 = new Point(2, 1, '#');
-            Point p1 = new Point(1, 2, 'o');*/
-            //Point.Draw(p1);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.SetBufferSize(120, 30);
 
-            List<Point> pointList = new List<Point>();
+			Walls walls = new Walls(120, 30);
+			walls.Draw();
 
-            pointList.Add(new Point(1, 2, '*'));
-            pointList.Add(new Point(3, 4, '*'));
-            pointList.Add(new Point(3,2,'*'));
-            pointList.Add(new Point(1, 4, '*'));
-            foreach (Point p1 in pointList)
-            {
-                p1.Draw();
-            }
+			// Отрисовка точек			
+			Point p = new Point(4, 5, '*');
+			Snake snake = new Snake(p, 4, Direction.RIGHT);
+			snake.Draw();
 
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-            upLine.Drow();
-            downLine.Drow();
-            leftLine.Drow();
-            rightLine.Drow();
+			FoodCreator foodCreator = new FoodCreator(120, 30, '$');
+			Point food = foodCreator.CreateFood();
+			food.Draw();
 
-            Point p = new Point(4, 5, 'x');
-            Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Drow();
+			while (true)
+			{
+				if (walls.IsHit(snake) || snake.IsHitTail())
+				{
+					break;
+				}
+				if (snake.Eat(food))
+				{
+					food = foodCreator.CreateFood();
+					food.Draw();
+				}
+				else
+				{
+					snake.Move();
+				}
+
+				Thread.Sleep(100);
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey(key.Key);
+				}
+			}
+			WriteGameOver();
+			Console.ReadLine();
+		}
 
 
-            Console.ReadLine();
-        }
+		static void WriteGameOver()
+		{
+			int xOffset = 25;
+			int yOffset = 8;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.SetCursorPosition(xOffset, yOffset++);
+			WriteText("============================", xOffset, yOffset++);
+			WriteText("ИГРА ОКОНЧЕНА", xOffset + 1, yOffset++);
+			yOffset++;
+			WriteText("Автор: Alisa Krupenko", xOffset + 2, yOffset++);
+			WriteText("============================", xOffset, yOffset++);
+		}
 
-    }
+		static void WriteText(String text, int xOffset, int yOffset)
+		{
+			Console.SetCursorPosition(xOffset, yOffset);
+			Console.WriteLine(text);
+		}
+
+	}
 }
 
 
